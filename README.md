b60e36e5c29b55111c7403bd5a1fe461229b39b4
unknown <mike6010550105@icloud.com>
2021-10-09 15:41:54 +0800

    10/9A

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # LSTMpaper_datapre
 論文前置
 normalize 統一資料規格
+300k_500_200iter
+300k_500iter
+1M amazon : mon10 predict mon15
+100k fox : mon10 predict mon15
--- T_compare.py
+++ T_compare.py
@@ -5,17 +5,8 @@ from sklearn.metrics import ndcg_score
 
 def read_data(filename):
     df = pd.read_csv(filename,
-                   sep='\t', names=['user','item','rate','time'], engine='python', encoding='latin-1')
-    matrix = df.pivot(index='user', columns='item', values='rate')
-    matrix.fillna(0, inplace=True)
-    matrix_list = matrix.iloc[:,:].values
-
-    return matrix_list
-
-def read_data_fox(filename):
-    df = pd.read_csv(filename,
                    sep=',', names=['user','item','rate','time'], engine='python', encoding='latin-1')
-    matrix = df.pivot(index='user', columns='item', values='rate')
+    matrix = df.pivot_table(index='user', columns='item', values='rate')
     matrix.fillna(0, inplace=True)
     matrix_list = matrix.iloc[:,:].values
 
@@ -33,11 +24,43 @@ def rate_rmse(predict, ori):
 
     return np.sqrt(error/len(xs))
 
+def recall(predict, ori):
+    """
+    A function to compute the total mean square error
+    """
+    xs, ys = ori.nonzero()
+    fn = 0
+    tp = 0
+    for x, y in zip(xs, ys):
+        error += pow(ori[x, y] - predict[x, y], 2)
+
+    return np.sqrt(error/len(xs))
+
+def precision(predict, ori):
+    """
+    A function to compute the total mean square error
+    """
+    xs, ys = ori.nonzero()
+    fp = 0
+    tp = 0
+    for x, y in zip(xs, ys):
+        error += pow(ori[x, y] - predict[x, y], 2)
+
+    return np.sqrt(error/len(xs))
+
 def check_dif(ori, next_month):
 
-    for i in range(len(ori)):
-        for j in range(len(ori[0])):
+    for i in range(ori.shape[0]):
+        for j in range(ori.shape[1]):
             if next_month[i][j] == ori[i][j]:
+                next_month[i][j] = 0
+    return next_month
+
+def clean_dif(ori, next_month):
+
+    for i in range(ori.shape[0]):
+        for j in range(ori.shape[1]):
+            if ori[i][j] != 0:
                 next_month[i][j] = 0
     return next_month
 
@@ -82,49 +105,62 @@ def min(ori, _):
         ori[i] = ori[i] - m
     return ori 
 
+    ####################################### NDCG #######################################################
+def ndcg():
+    count = 0
+    orindcg = 0
+    singlendcg = 0
+    for i in range(ori.shape[0]):
+        if np.count_nonzero(first[i]) >= 50:
+            count = count + 1
+            orindcg = orindcg + ndcg_score(np.asarray([first[i]]), np.asarray([ori_mf[i]]), k=10)
+            singlendcg = singlendcg + ndcg_score(np.asarray([first[i]]), np.asarray([singletrain_mf[i]]), k=10)
+    print(orindcg/count, singlendcg/count, count)
+    return 0
+    ####################################### NDCG #######################################################
+
+    ####################################### H #######################################################
+    ####################################### H #######################################################
+
+    ####################################### P #######################################################
+    ####################################### P #######################################################
+
+    ####################################### R #######################################################
+def rmse():
+    ori_rmse = rate_rmse(ori_mf, first)
+    single_rmse = rate_rmse(singletrain_mf, first)
+    print(ori_rmse, single_rmse)
+    return 0
+    ####################################### R #######################################################
+
+
 # for i in range(15, 36):
 #     data_path1 = '1M-data/'
 #     ori = read_data(data_path1 + f'a{i}.tsv')
 #     next_month = read_data(data_path1 + f'a{i+1}.tsv')
 #     first = check_dif(ori, next_month)
 #     print(rate_rmse(ori_mf, first), rate_rmse(singletrain_mf, first))
-list_ = [100, 150, 200, 250, 300]
+list_ = [300]
 V_lstm = []
 V_lstm_delta = []
-dataset = "1M"
-# D = [1.014014975, 0.9965458088, 0.9897703265, 0.9911442422, 0.9925687081]
-# D1 = [1.01407542, 0.9965985365, 0.9891604569, 0.9905492221, 0.9945555019]
-# D2 = [1.013876322, 0.9966166909, 0.9889331608, 0.987998119, 0.9928028801]
-dataset_path = f'MF_lstm_datapreprocess/compare_{dataset}dataset/'
+dataset = "fox"
+
 for i, k in enumerate(list_):
-    ori_path = f'{dataset}-result-orimf_{k}k_500iter_'
-    single_path = f'{dataset}-result-single500-{k}-{k}k_'
-    ori_mf = np.load(dataset_path + ori_path + 'matrixa10.tsv.npy')
-    singletrain_mf = np.load(dataset_path + single_path + 'matrixa10.tsv.npy')
-    data_path1 = f'MF_lstm_datapreprocess/{dataset}/'
-    ori = read_data(data_path1 + f'a10.tsv')
-    next_month = read_data(data_path1 + f'a36.tsv')
+    ori_mf = np.load(f'{dataset}-oriresult/' + dataset +'_mon3.npy')
+    singletrain_mf = np.load(f'{dataset}-singleresult/' + dataset + '_mon3.npy')
+    ori = read_data(f'{dataset}/' + dataset + '_mon3.csv')
+    next_month = read_data(f'{dataset}/' + dataset + '_mon8.csv')
+
     first = check_dif(ori, next_month)
+    ori_mf = clean_dif(ori, ori_mf)
+    singletrain_mf = clean_dif(ori, singletrain_mf)
+
+    rmse()
+
     # print(ori_mf.shape, singletrain_mf.shape, first.shape, next_month.shape)
     # ori = rate_rmse(ori_mf, first)
     # single = rate_rmse(singletrain_mf, first)
-    
     # month = approximate(singletrain_mf)
-    
-    count = 0
-    orindcg = 0
-    singlendcg = 0
-    for i in range(955):
-        if np.count_nonzero(next_month[i]) >= 100:
-            count = count + 1
-            orindcg = orindcg + ndcg_score([list(next_month[i])], [list(ori_mf[i])], k=10)
-            singlendcg = singlendcg + ndcg_score([list(next_month[i])], [list(singletrain_mf[i])], k=10)
-            # print(i)
-            # print(next_month[i])
-            # print(np.nonzero(next_month[i]))
-    print(next_month[0], singletrain_mf[0])
-    print(orindcg/count, singlendcg/count, count)
-    # next_month = list(next_month[1])
 
     # single = ori-(single-ori)  ###############100k fox
     # single = single*D2[i]
